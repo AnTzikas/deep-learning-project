0d6053731384e36e6d30e34b1a2fe4d4c60d8cda
antzikas <antzikas@uth.gr>
2025-06-14 18:07:40 +0300

    script_12

--- script_12.sh
+++ script_12.sh
@@ -2,11 +2,12 @@
 
 TEACHER="Pretrain/ml-100k_NeuMF_32_[64,32,16,8]_1749891631.npy"
 TECHNIQUE="response"  # change to: response, feature, mlp_student, relation
-LAYERS="[16,8]"     # you can change it here
+LAYERS="[8]"     # you can change it here
+REG_LAYERS="[0]"
 FACTORS=8              # embedding size for student
-LOGFILE="logs/${TECHNIQUE}.txt"
+LOGFILE="logs${LAYERS}/${TECHNIQUE}.txt"
 
-mkdir -p logs
+mkdir -p logs${LAYERS}
 
 #response
 for i in {1..10}
@@ -17,6 +18,7 @@ do
     --technique "$TECHNIQUE" \
     --layers "$LAYERS" \
     --num_factors "$FACTORS" \
+    --reg_layers "$REG_LAYERS" \
     --epochs 11 \
     --out 0 \
     | tee -a $LOGFILE
@@ -24,11 +26,12 @@ done
 
 #feature
 TECHNIQUE="feature"
-LOGFILE="logs/${TECHNIQUE}.txt"
+LOGFILE="logs${LAYERS}/${TECHNIQUE}.txt"
 for i in {1..10}
 do
   echo "Run $i" | tee -a $LOGFILE
   python ex_12.py \
+    --reg_layers "$REG_LAYERS" \
     --teacher_model "$TEACHER" \
     --technique "$TECHNIQUE" \
     --layers "$LAYERS" \
@@ -40,11 +43,12 @@ done
 
 #student_mlp
 TECHNIQUE="relation"
-LOGFILE="logs/${TECHNIQUE}.txt"
+LOGFILE="logs${LAYERS}/${TECHNIQUE}.txt"
 for i in {1..10}
 do
   echo "Run $i" | tee -a $LOGFILE
   python ex_12.py \
+    --reg_layers "$REG_LAYERS" \
     --teacher_model "$TEACHER" \
     --technique "$TECHNIQUE" \
     --layers "$LAYERS" \
